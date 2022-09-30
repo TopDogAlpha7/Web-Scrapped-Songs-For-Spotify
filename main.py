16240afc7c8fa08e434ec9a0e5c4c2f5c191f459
Tinodaishe Simbanegavi <108521409+TopDogAlpha7@users.noreply.github.com>
2022-09-30 18:44:51 +0200

    added file

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import lxml
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+REDIRECT_URI = "https://open.spotify.com/"
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get("client_id"), client_secret=os.environ.get("client_secret"), redirect_uri=REDIRECT_URI, show_dialog=True, cache_path="cache.txt", scope="playlist-modify-private"))
+
+print("Enter the data in the format YYYY-MM-DD")
+
+year = input("Enter year: ")
+month = input("Enter month: ")
+day = input("Enter day: ")
+date = f"{year}-{month}-{day}"
+
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}")
+txt = response.text
+
+soup = BeautifulSoup(txt, "lxml")
+title = soup.find_all(name="h3", id="title-of-a-story", class_="u-line-height-125")
+all_titles = [song.getText().replace('\n\n\t\n\t\n\t\t\n\t\t\t\t\t', '').replace('\t\t\n\t\n', '') for song in title]
+
+uri_list = []
+for song in all_titles:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        uri_list.append(uri)
+    except IndexError:
+        pass
+
+playlist = sp.user_playlist_create(user=os.environ.get("user_id"), name="Web scrapped bangers", public=False)
+playlist_id = playlist["id"]
+sp.playlist_add_items(playlist_id=playlist_id, items=uri_list)
+
